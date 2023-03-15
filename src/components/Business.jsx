71969b7c4851b02f71acec6e1f9472452d4dbc11
Alexaldi <gilangaldiano05@gmail.com>
2023-03-16 03:08:38 +0700

    upload new design and final bug

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,25 +2,16 @@ import Cookies from "js-cookie";
 import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
-
-const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
-    </div>
-    <div className="flex-1 flex flex-col ml-3">
-      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
-        {title}
-      </h4>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
-        {content}
-      </p>
-    </div>
-  </div>
-);
+import { useEffect, useState } from "react";
 
 const Business = () => {
-  const token = JSON.parse(atob(Cookies.get("Siswa")))
+  const [token, setToken] = useState({});
+  useEffect(() => {
+    const data = Cookies.get("Siswa")
+    if (data !== undefined) {
+      setToken(JSON.parse(data));
+    }
+  }, [])
   return (
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
